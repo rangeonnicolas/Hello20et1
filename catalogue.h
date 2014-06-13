edf1eda93da7d71be725c5100fc0d733f726d639
rangeonnicolas <nicapps23@gmail.com>
2014-06-13 15:06:13 +0200

    mergeage2

---
+++ catalogue.h
@@ -0,0 +1,26 @@
+#ifndef CATALOGUE_H
+#define CATALOGUE_H
+
+#include <QDialog>
+#include "UV.h"
+
+using namespace UV_credits_types;
+
+namespace Ui {
+    class catalogue;
+}
+
+class catalogue : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit catalogue(UV& uv,QWidget *parent = 0);
+    ~catalogue();
+
+private:
+    Ui::catalogue *ui;
+};
+
+
+#endif // CATALOGUE_H
